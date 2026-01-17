097dd0bdbadaec5185bce8629e61966ad9cebabe
Parmita <160781867+kabbo1031@users.noreply.github.com>
2026-01-17 19:25:03 +0600

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,5 @@
+function updateUI(data) {
+    document.getElementById("slow").innerText = data.slow;
+    document.getElementById("normal").innerText = data.normal;
+    document.getElementById("fast").innerText = data.fast;
+}
